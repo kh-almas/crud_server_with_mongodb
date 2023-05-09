c9ba7d02edf45f9825006d0f1add82980c5b825a
kh-almas <services.almas@gmail.com>
2023-05-09 23:43:10 +0600

    install tailwindcss and small update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [user, setUser] = useState([]);
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ name, age, email });
+        // You can perform additional actions here, like submitting the form data to a server
+    };
+
+      useEffect( () => {
+          fetch('http://localhost:4000/')
+              .then(res => res.json())
+              .then(data => setUser(data))
+      }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+            <div className="mb-4">
+                <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
+                    Name
+                </label>
+                <input
+                    type="text"
+                    id="name"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="age" className="block text-gray-700 font-bold mb-2">
+                    Age
+                </label>
+                <input
+                    type="number"
+                    id="age"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                    required
+                />
+            </div>
+            <div className="mb-6">
+                <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+                    Email
+                </label>
+                <input
+                    type="email"
+                    id="email"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </div>
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+                Submit
+            </button>
+        </form>
+        <h1>Total person: {user.length}</h1>
+        {
+            user.map(data => <p key={data.id}>{data.name}</p>)
+        }
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const cors = require('cors');
+
+//middleware
+app.use(cors());
 
 const people = [
     {
+        "id": 452,
         "name": "John Smith",
         "age": 25,
         "email": "john.smith@example.com"
     },
     {
+        "id": 453,
         "name": "Jane Doe",
         "age": 30,
         "email": "jane.doe@example.com"
     },
     {
+        "id": 454,
         "name": "Bob Johnson",
         "age": 40,
         "email": "bob.johnson@example.com"
