3bae99316ca3b8656700fcdd7b85b7f88b770363
kh-almas <services.almas@gmail.com>
2023-05-10 12:20:14 +0600

    connect with mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
-const cors = require('cors');
+const mongodb_username = process.env.MONGODB_USERNAME;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+
+
 
 //middleware
 app.use(cors());
@@ -27,6 +33,33 @@ const people = [
         "email": "bob.johnson@example.com"
     }
 ];
+
+
+const uri = `mongodb+srv://${mongodb_username}:${mongodb_password}@cluster.9zce0xe.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
